236613412b5c83ac9e42e67549c2177b1ff58de5
虎正玺 <15264316179@163.com>
2025-08-28 01:32:44 +0800

    chore: 调整打包参数

--- src/define.rs
+++ src/define.rs
@@ -1,8 +1,8 @@
 // 定义授权时间戳
-pub const AUTH_START_TIMESTAMP: i64 = 1756285993;
+pub const AUTH_START_TIMESTAMP: i64 = 1756315589;
 
 // 定义授权码过期时间—— 1 小时
-pub const AUTH_CODE_EXPIRES: i64 = 60 * 60;
+pub const AUTH_CODE_EXPIRES: i64 = 60 ;
 
 // 定义提示内容 “授权已过期，请联系开发者 —— 185 0038 3830”
 
