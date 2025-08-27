33637228a51a71986ec2de7e155d49e88ef607fa
Tiger <15264316179@163.com>
2025-08-27 13:12:08 +0800

    feat(auth): 新增授权码过期检测功能
    - 添加授权时间戳常量和授权码有效期常量
    - 引入 js-sys 和 web-sys 库以支持时间获取和窗口操作
    - 实现检查授权码是否过期的内部函数
    - 过期时弹窗提示授权已过期并联系管理员
    - greet 函数中集成过期检查并打印状态日志
    - 在代码中开启调试日志输出过期时间和时间差信息

---
+++ src/define.rs
@@ -0,0 +1,11 @@
+// 定义授权时间戳
+pub const AUTH_START_TIMESTAMP: i64 = 1756271401;
+
+// 定义授权码过期时间—— 1 分钟
+pub const AUTH_CODE_EXPIRES: i64 = 60;
+
+// 定义提示内容 “授权已过期，请联系管理员 (+86)185 0038 3830”
+
+pub const AUTH_CODE_EXPIRED_CONTENT: &'static str = "授权已过期，请联系管理员 (+86)185 0038 3830";
+
+pub  const  IS_DEBUG: bool = true;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use wasm_bindgen::prelude::*;
 
+// 模块声明
+mod define;
+
+// // 公开导出常量，供其他文件使用
+use define::{AUTH_START_TIMESTAMP, AUTH_CODE_EXPIRES, AUTH_CODE_EXPIRED_CONTENT, IS_DEBUG};
+
 // 导入 `console.log` 函数从 `web-sys` crate
 #[wasm_bindgen]
 extern "C" {
@@ -12,10 +18,43 @@ macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
+
+// 调用 window.alert
+macro_rules! alert {
+    ($msg:expr) => {{
+        web_sys::window()
+            .expect("no global `window` exists")
+            .alert_with_message($msg)
+            .expect("should have been able to alert");
+    }}
+}
+
+
+// 定义内部方法。检查授权码是否过期，检查方法是获取当前时间戳并和授权码的过期时间进行比较。
+fn check_auth_code_expired() -> bool {
+    // 获取当前时间戳（毫秒）
+    let now = js_sys::Date::now();
+    // 转换为秒
+    let seconds = (now / 1000.0) as u64;
+    let expired_time = (AUTH_START_TIMESTAMP + AUTH_CODE_EXPIRES) as u64;
+    let diff: i64 = (seconds - expired_time) as i64;
+    if IS_DEBUG {
+        console_log!("当前时间戳: {}, 过期时间戳：{}", seconds, expired_time);
+        // 打印 diff，打印 u64 类型
+        console_log!("diff: {}", diff);
+    }
+
+    if diff > 0 {
+        alert!(AUTH_CODE_EXPIRED_CONTENT)
+    }
+    return diff < 0;
+}
+
 // 导出一个 `greet` 函数从 Rust 到 JavaScript
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    console_log!("Hello, {}!", name);
+    let is_expired = check_auth_code_expired();
+    console_log!("Hello, {}!，{}", name, if is_expired { "没过期" } else { "过期了" });
 }
 
 // 添加两个数字的函数
